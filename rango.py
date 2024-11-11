576f6305de831325631e0ca12a1307a7a266a44c
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-11-11 12:13:37 +0100

    Use letter list instead of capture

--- rango.py
+++ rango.py
@@ -85,13 +85,13 @@ ctx.lists["user.rango_page"] = {
 
 
 @mod.capture(
-    rule="<user.letter> | <user.letter> <user.letter> | <user.rango_hint_double>"
+    rule="{user.letter} | {user.letter} {user.letter} | <user.rango_hint_double>"
 )
 def rango_hint(m) -> str:
     return "".join(m)
 
 
-@mod.capture(rule="<user.letter> (twice | second)")
+@mod.capture(rule="{user.letter} (twice | second)")
 def rango_hint_double(m) -> str:
     return m.letter + m.letter
 
